6bce954d4a92cc32893031b6ac31c33943362188
alidrisy <a.alidrisy6@gmail.com>
2023-09-22 11:19:16 -0700

    add

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Creates a file in /tmp
 
 file { '/tmp/school':
-	ensure => 'file',
-	owner => 'www-data',
-	group => 'www-data',
-	mode  => '0744',
-	content => "I love Puppet",
+  ensure  => 'file',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
   }
