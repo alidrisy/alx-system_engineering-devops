1b85fdb63da7cc00e028d4c36b2de10bc30f4e52
alidrisy <a.alidrisy6@gmail.com>
2023-09-26 22:24:51 +0300

    fix it

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,8 +1,9 @@
 # Setup New Ubuntu server with nginx
 
 exec {'update env':
-  command => '/usr/bin/apt-get update',
+  command => '/usr/bin/apt-get update'
   }
+
 pakage {'nginx':
   ensure => 'installed',
   require => Exec['update env']
@@ -13,13 +14,12 @@ file {"/var/www/html/index.nginx-debian.html":
   content => 'Hello World!
   }
 
-#exec {'redirect_me':
- # command => 'sed -i "24i\        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-  #provider => 'shell'
-#}
+exec {'redirect_me':
+  command => 'sed -i "24i\        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell'
+  }
 	  
 service {'nginx':
   ensure => running,
-  Restart => true,
   require => Package['nginx']
   }
