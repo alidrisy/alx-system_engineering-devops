58b97e1ddb46f616a9a1aa6643cde9cc1e798350
alidrisy <a.alidrisy6@gmail.com>
2023-09-26 21:08:20 +0300

    Install Nginx web server (w/ Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,31 @@
+exec {'update':
+  command => 'apt-get -y update',
+  provider => 'shell',
+  }
+
+pakage {'nginx':
+  provider => 'apt-get',
+  install_options =>[ '-y' ],
+  }
+
+
+file {"/var/www/html/index.nginx-debian.html":
+  ensure => 'file',
+  owner => '$USER',
+  group =>  '$USER',
+  mode => '0744',
+  content => 'Hello World!,
+  }
+
+file_line {'set redirect':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  after  => 'listen 80 default_server;',
+  }
+	  
+service {'nginx':
+  ensure => 'running'
+  enable => true,
+  Restart => true,
+  }
