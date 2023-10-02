dc5e93439fae6c3df789154fa187a8eedccb1263
alidrisy <a.alidrisy6@gmail.com>
2023-10-03 01:09:45 +0300

    add alx pup-key

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,23 +4,14 @@
  exec { 'update system': 
          command => '/usr/bin/apt-get update', 
  } 
-  
- package { 'nginx': 
-         ensure => 'installed', 
-         require => Exec['update system'] 
- } 
-  
- file {'/var/www/html/index.html': 
-         content => 'Hello World!' 
- } 
-  
- exec {'redirect_me': 
-         command => 'sed -i "24i\        rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default', 
-         provider => 'shell' 
- } 
+
+package { 'nginx':
+         ensure => 'installed',
+         require => Exec['update system']
+ }
   
  exec {'HTTP header': 
-         command => 'sed -i "25i\        add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default', 
+         command => 'sed -i "25i\\tadd_header X-Served-By \$hostname;" /etc/nginx/sites-available/default', 
          provider => 'shell' 
  } 
   
