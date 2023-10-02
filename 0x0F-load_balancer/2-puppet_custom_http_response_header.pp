407cd4d029beb38c91e4d783a75f8cc01738c6ca
alidrisy <a.alidrisy6@gmail.com>
2023-10-03 00:11:37 +0300

    fix errors

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,3 +4,7 @@ exec {'custom_HTTP':
 	command => 'sed -i '26i\\tadd_header X-Served-By $hostname;' /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
+
+service {'nginx':
+	ensure => running,
+}
