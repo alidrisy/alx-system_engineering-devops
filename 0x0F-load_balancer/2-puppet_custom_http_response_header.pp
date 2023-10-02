24f0d3aedee84bec9812c72320a6a5722efeb62b
alidrisy <a.alidrisy6@gmail.com>
2023-10-03 00:21:23 +0300

    fix errors

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,24 @@
 #Creating a custom HTTP header response
 
+exec { 'update system':
+	command => 'apt-get -y update',
+	provider => 'shell'
+}
+
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
 exec {'custom_HTTP':
 	command => 'sed -i '26i\\tadd_header X-Served-By $hostname;' /etc/nginx/sites-available/default',
 	provider => 'shell'
@@ -7,4 +26,5 @@ exec {'custom_HTTP':
 
 service {'nginx':
 	ensure => running,
+	require => Package['nginx']
 }
