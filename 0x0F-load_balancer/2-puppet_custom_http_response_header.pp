2b02d49de135d7dd9e9a7a4701e0999c765f0057
alidrisy <a.alidrisy6@gmail.com>
2023-10-03 11:36:47 +0300

    fix errors

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -15,12 +15,12 @@
  } 
   
  exec {'redirect_me': 
-         command => 'sed -i "24i\\trewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default', 
+         command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default', 
          provider => 'shell' 
  } 
   
  exec {'HTTP header': 
-         command => 'sed -i "25i\\tadd_header X-Served-By \$hostname;" /etc/nginx/sites-available/default', 
+         command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default', 
          provider => 'shell' 
  } 
   
