cf3104d0e56c37cd15152634be532010c3c2ed42
alidrisy <a.alidrisy6@gmail.com>
2023-11-07 23:44:47 +0300

    3. Count it!

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Model to queries the Reddit API"""
+import requests
+
+
+def count_words(subreddit, word_list):
+    """ Returns a list containing the titles of all hot articles
+    for a given subreddit """
+    try:
+        url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+        params = {'after': after, 'limit': 100, 'count': count}
+        response = requests.get(url, params=params, allow_redirects=False)
+        if response.status_code == '404':
+            return None
+        data = response.json()
+        for post in data['data']['children']:
+            hot_list.append(post['data']['title'])
+        after = data['data']['after']
+        count = data['data']['dist']
+        if after is not None:
+            recurse(subreddit, hot_list, after, count)
+        return hot_list
+    except Exception:
+        return None
