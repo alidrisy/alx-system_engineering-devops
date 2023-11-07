bff9ced165370f83ec9a48f38a2a8347fc144bbe
alidrisy <a.alidrisy6@gmail.com>
2023-11-07 23:33:26 +0300

    fix error

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,12 +4,13 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """ Queries the Reddit API and returns a list containing the
-    titles of all hot articles for a given subreddit """
+    """ Returns a list containing the titles of all hot articles
+    for a given subreddit """
     try:
         url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
         params = {'after': after, 'limit': 100, 'count': count}
-        response = requests.get(url, params=params, headers=headers, allow_redirects=False)
+        response = requests.get(url, params=params, headers=headers,
+                                allow_redirects=False)
         if response.status_code == '404':
             return None
         data = response.json()
@@ -22,4 +23,3 @@ def recurse(subreddit, hot_list=[], after="", count=0):
         return hot_list
     except Exception:
         return None
-
