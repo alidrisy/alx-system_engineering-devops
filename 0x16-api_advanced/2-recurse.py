c27f8c256a5631f19c48bbf68f408d4aa54e96da
alidrisy <a.alidrisy6@gmail.com>
2023-11-07 23:13:33 +0300

    2. Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,11 +7,12 @@ def recurse(subreddit, hot_list=[], after=""):
     """ Queries the Reddit API and returns a list containing the
     titles of all hot articles for a given subreddit """
     try:
-        if after:
+        if after != "":
             url = 'https://www.reddit.com/r/{}/hot.json?limit=100&after={}'\
                     .format(subreddit, after)
         else:
-            url = 'https://www.reddit.com/r/{}/hot.json?limit=100'.format(subreddit)
+            url = 'https://www.reddit.com/r/{}/hot.json?limit=100'\
+                    .format(subreddit)
         response = requests.get(url, allow_redirects=False)
         if response.status_code == '404':
             return None
