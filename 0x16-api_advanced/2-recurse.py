f120455135e3d559ea69e8455cc4d8a77a8d18a6
alidrisy <a.alidrisy6@gmail.com>
2023-11-07 23:29:04 +0300

    2. Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,16 +6,20 @@ import requests
 def recurse(subreddit, hot_list=[], after="", count=0):
     """ Queries the Reddit API and returns a list containing the
     titles of all hot articles for a given subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    params = {'after': after, 'limit': 100, 'count': count}
-    response = requests.get(url, params=params, headers=headers, allow_redirects=False)
-    if response.status_code == '404':
+    try:
+        url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+        params = {'after': after, 'limit': 100, 'count': count}
+        response = requests.get(url, params=params, headers=headers, allow_redirects=False)
+        if response.status_code == '404':
+            return None
+        data = response.json()
+        for post in data['data']['children']:
+            hot_list.append(post['data']['title'])
+        after = data['data']['after']
+        count = data['data']['dist']
+        if after is not None:
+            recurse(subreddit, hot_list, after, count)
+        return hot_list
+    except Exception:
         return None
-    data = response.json()
-    for post in data['data']['children']:
-        hot_list.append(post['data']['title'])
-    after = data['data']['after']
-    count = data['data']['dist']
-    if after is not None:
-        recurse(subreddit, hot_list, after, count)
-    return hot_list
+
