26cb738262bb9b4745259040ead6e3fdff6a546b
alidrisy <a.alidrisy6@gmail.com>
2023-11-07 20:33:18 +0300

    1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Model to queries the Reddit API"""
+import requests
+
+
+def top_ten(subreddit):
+    """ Queries the Reddit API and returns a list containing the
+    titles of all hot articles for a given subreddit. """
+    try:
+        response = requests.get('https://www.reddit.com/r/{}/about.json'
+                                .format(subreddit), allow_redirects=False)
+        if response.status_code == '404':
+            print(None)
+        data = response.json()
+        titles = []
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    except Exception:
+        print(None)
