0dfb4f2a94db732e71c798358016daddd2e164ec
alidrisy <a.alidrisy6@gmail.com>
2023-11-09 01:24:51 +0300

    0. Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # find out why Apache is returning a 500 error.  and fix it
- 
- exec {'Fix apache2': 
-   	 command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php', 
-   	 provider => shell, 
- }
+
+  exec {'Fix apache2':
+      command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+      provider => shell,
+  }
