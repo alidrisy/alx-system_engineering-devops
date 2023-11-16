0148e7392715ab341c656de313e89c21cd69a97e
alidrisy <a.alidrisy6@gmail.com>
2023-11-16 20:37:35 +0300

    fix files limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,7 @@
 # Fix nginx limit open files
 
-exec {
-    command => "sed -i 's/15/2000/g' /etc/default/nginx",
-    provider => 'shell'
+exec {'change file open limit':
+    command  => 'sed -i "s/15/2000/g" /etc/default/nginx',
+    provider => shell,
+    onlyif   => 'test -f /etc/default/nginx'
 }
